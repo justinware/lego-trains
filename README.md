2c4e9d88120d7822c67d51324991e2021c0bdc7d
Justin Ware <wareisjustin@gmail.com>
2019-11-29 12:57:52 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lego Trains
+[![Build Status](https://travis-ci.com/justinware/lego-trains.svg?branch=master)](https://travis-ci.com/justinware/lego-trains)
---
+++ gulpfile.ts
@@ -0,0 +1,28 @@
+import { task, src, dest, series, parallel } from 'gulp';
+import * as ts from 'gulp-typescript';
+import * as del from 'del';
+import * as chalk from 'chalk';
+
+const tsProject = ts.createProject('tsconfig.json');
+const outputDir = 'dist';
+const logMessagePrefix = '         +  ';
+const logMessage = (action: string, context: string) => {
+
+  console.log(`${logMessagePrefix}${action}${chalk.magenta(context)}`);
+};
+
+task('clean', async () => {
+
+  const paths = await del([outputDir]);
+  
+  logMessage('Deleted ', paths.join('; '));
+});
+
+task('compile', () => {
+
+  const tsResult = src('src/**/*.ts').pipe(tsProject());
+  
+  return tsResult.js.pipe(dest(outputDir));
+});
+
+task('build', series('clean', parallel(['compile'])));
