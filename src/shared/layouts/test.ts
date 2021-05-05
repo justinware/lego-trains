d27709f12ef2c2206b43089c20e466c15bc7be26
Justin Ware <wareisjustin@gmail.com>
2021-05-05 22:03:09 +1000

    Added TrackSwitch to Test layout.

--- src/shared/layouts/test.ts
+++ src/shared/layouts/test.ts
@@ -2,11 +2,17 @@ import Layout from '../core/layout';
 import Board from '../core/board';
 import Train from '../core/train';
 import MotionSensor from '../core/motionSensor';
-import { IBoard, ITrain, IMotionSensor } from '../types';
+import TrackSwitch from '../core/trackSwitch';
+import { IBoard, ITrain, IMotionSensor, ITrackSwitch } from '../types';
 import { TrainDirection, TrainSpeed, SensorEvent } from '../types/enums';
 import wait from '../utils/wait';
 import { WAIT_TIMES } from '../core/constants';
 import runTrainUntilSensor from '../actions/runTrainUntilSensor';
+
+const TRAIN_PARK_SPEED = TrainSpeed.Low;
+const TRAIN_PARK_DURATION = WAIT_TIMES.MEDIUM;
+const TRAIN_RUN_SPEED = TrainSpeed.Medium;
+const TRAIN_RUN_DURATION = WAIT_TIMES.LARGE;
 
 class TestLayout extends Layout {
 
@@ -14,6 +20,7 @@ class TestLayout extends Layout {
   private _train: ITrain;
   private _sensor1: IMotionSensor;
   private _sensor2: IMotionSensor;
+  private _switch: ITrackSwitch;
 
   protected async initialise(): Promise<void> {
 
@@ -21,34 +28,50 @@ class TestLayout extends Layout {
     this._train = new Train(1, 5, 6, this._isDummy);
     this._sensor1 = new MotionSensor(1, 'A1', this._isDummy);
     this._sensor2 = new MotionSensor(2, 'A2', this._isDummy);
+    this._switch = new TrackSwitch(1, 'A0', this._isDummy);
 
     await this._board.initialise();
+    await this._switch.initialise();
 
-    // Initialise any track switches
+    if (this._isDummy) {
 
-    await wait(WAIT_TIMES.SMALL);
+      await wait(WAIT_TIMES.SMALL);
+    }
   }
 
   protected async executeStart(): Promise<void> {
 
     if (this._isDummy) {
 
-      setTimeout(() => { this._sensor2.forcePumpExit(); }, WAIT_TIMES.LARGE);
+      setTimeout(() => { this._sensor2.forcePumpExit(); }, TRAIN_PARK_DURATION);
     }
 
-    await runTrainUntilSensor(this._train, TrainDirection.Forward, TrainSpeed.Low, this._sensor2, SensorEvent.Exit);
+    await runTrainUntilSensor(this._train, TrainDirection.Forward, TRAIN_PARK_SPEED, this._sensor2, SensorEvent.Exit);
+    await this._switch.turn();
   }
 
   protected async executeLoop(): Promise<void> {
 
-    // Move Train forward at medium speed, when Sensor 2 pumps Exit, then resolve
-    this._train.move(TrainDirection.Forward, TrainSpeed.Medium);
+    if (this._isDummy) {
+
+      setTimeout(() => { this._sensor2.forcePumpExit(); }, TRAIN_RUN_DURATION);
+    }
+
+    await runTrainUntilSensor(this._train, TrainDirection.Forward, TRAIN_RUN_SPEED, this._sensor2, SensorEvent.Exit);
   }
 
   protected async executeEnd(): Promise<void> {
 
-    // Move Train backwards slowly until Sensor 1 pumps Exit, then stop the train, then resolve
-    this._train.move(TrainDirection.Backward, TrainSpeed.Low);
+    this._train.stop();
+    await this._switch.straight();
+
+    if (this._isDummy) {
+
+      setTimeout(() => { this._sensor1.forcePumpExit(); }, TRAIN_PARK_DURATION);
+    }
+
+    await runTrainUntilSensor(this._train, TrainDirection.Backward, TRAIN_PARK_SPEED, this._sensor1, SensorEvent.Exit);
+    this._train.stop();
   }
 }
 
